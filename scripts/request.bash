91ae014ebea049b7d4933d6c4837aaa8dc646649
natancs <natan.carvalho123@hotmail.com>
2024-02-12 22:23:07 -0300

    scripts: updating the file request.bash

--- scripts/request.bash
+++ scripts/request.bash
@@ -1,5 +1,5 @@
 # Create a new product_code
-curl -X POST http://0.0.0.0:3000/create_tag -H 'Content-Type: application/json' -d '{"product_code":"teste"}'
+curl -X POST http://0.0.0.0:3000/create_tag -H 'Content-Type: application/json' -d '{"product_code":"teste"}' -i
 
 # Create validation
-curl -X POST http://0.0.0.0:3000/create_tag -H 'Content-Type: application/json' -d '{"product_cod":"teste"}'
+curl -X POST http://0.0.0.0:3000/create_tag -H 'Content-Type: application/json' -d '{"product_cod":"teste"}' -i
